b43047182c16ee751aaa2ac0e4ec88d0dfcd305d
matrizzafox <vas.matrizza@gmail.com>
2023-10-16 14:27:06 +0300

    ➕ADD: Pseudo fetch delay

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -2,9 +2,24 @@ import LoadingBottomCopyright from "@src/containers/LoadingBottomCopyright";
 import LoadingLogoSection from "@src/containers/LoadingLogoSection";
 import useLoaderStatus from "@src/hooks/useLoaderStatus";
 import LoadingLayout from "@src/layouts/LoadingLayout";
+import { setAppLoadedStatus } from "@src/store/slices/app.slice";
+import { useAppDispatch } from "@src/store/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoadingPage = () => {
   const { isError, isInitial, status } = useLoaderStatus();
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    isInitial &&
+      new Promise((resolve) => setTimeout(resolve, 3000)).then(() => {
+        dispatch(setAppLoadedStatus());
+        navigate("/");
+      });
+  }, [isInitial, dispatch, navigate]);
 
   return (
     <LoadingLayout>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,15 @@
+import { AppStateStatus, selectAppState } from "@src/store/slices/app.slice";
+import { useAppSelector } from "@src/store/store";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+  const { status } = useAppSelector(selectAppState);
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/loading");
-  }, [navigate]);
+    if (status === AppStateStatus.INITIAL) navigate("/loading");
+  }, [navigate, status]);
 
   return <div>Main Page</div>;
 };
