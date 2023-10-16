88ab1cdf067cf599b0909242efef5094c82cfe4a
matrizzafox <vas.matrizza@gmail.com>
2023-10-16 19:19:08 +0300

    ➕ADD: Icons

---
+++ src/ui/icons/RuIcon.tsx
@@ -0,0 +1,77 @@
+const RuIcon = () => {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.0003 27.5C20.1247 27.5 24.5235 24.4131 26.4528 20H3.54785C5.47723 24.4131 9.87597 27.5 15.0003 27.5Z"
+        fill="url(#paint0_linear_72_106)"
+      />
+      <path
+        d="M15.0003 2.5C9.87597 2.5 5.47723 5.58687 3.54785 10H26.4535C24.5235 5.58687 20.1247 2.5 15.0003 2.5Z"
+        fill="url(#paint1_linear_72_106)"
+      />
+      <path
+        d="M26.4525 10H3.5475C2.8775 11.5319 2.5 13.2212 2.5 15C2.5 16.7787 2.8775 18.4681 3.5475 20H26.4531C27.1225 18.4681 27.5 16.7787 27.5 15C27.5 13.2212 27.1225 11.5319 26.4525 10Z"
+        fill="url(#paint2_linear_72_106)"
+      />
+      <path
+        d="M26.4525 10H3.5475C2.8775 11.5319 2.5 13.2212 2.5 15C2.5 16.7787 2.8775 18.4681 3.5475 20H26.4531C27.1225 18.4681 27.5 16.7787 27.5 15C27.5 13.2212 27.1225 11.5319 26.4525 10Z"
+        fill="url(#paint3_linear_72_106)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_72_106"
+          x1="3.54598"
+          y1="9.15501"
+          x2="21.0085"
+          y2="26.6175"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#F44F5A" />
+          <stop offset="0.443" stopColor="#EE3D4A" />
+          <stop offset="1" stopColor="#E52030" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_72_106"
+          x1="9.07785"
+          y1="3.46875"
+          x2="26.5335"
+          y2="20.9244"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" />
+          <stop offset="0.998" stopColor="#9EA7B0" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_72_106"
+          x1="6.48562"
+          y1="6.48563"
+          x2="24.1106"
+          y2="24.1106"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="white" />
+          <stop offset="0.998" stopColor="#9EA7B0" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_72_106"
+          x1="6.25562"
+          y1="6.25563"
+          x2="23.8694"
+          y2="23.8694"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#4AB7FF" />
+          <stop offset="1" stopColor="#007AD9" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default RuIcon;
--- src/ui/icons/index.ts
+++ src/ui/icons/index.ts
@@ -1,2 +1,8 @@
 export { default as CloseIcon } from "./CloseIcon";
 export { default as HideIcon } from "./HideIcon";
+export { default as HomeIcon } from "./HomeIcon";
+export { default as SettingsIcon } from "./SettingsIcon";
+export { default as InfoIcon } from "./InfoIcon";
+export { default as EngIcon } from "./EngIcon";
+export { default as RuIcon } from "./RuIcon";
+export { default as DiscordIcon } from "./DiscordIcon";
