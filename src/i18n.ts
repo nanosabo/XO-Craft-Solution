6cd388a332d93b1251b07cc1e1335b85a2cc15f2
matrizzafox <vas.matrizza@gmail.com>
2023-10-16 19:55:14 +0300

    🔃UPD: Locales for production build

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,9 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+import { resources } from "./locales/resources";
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({ fallbackLng: "en", debug: true });
+  .init({ fallbackLng: "en", debug: true, resources });
---
+++ src/locales/resources.ts
@@ -0,0 +1,16 @@
+import loaderPageEn from "./en/loaderPage.json";
+import loaderPageRu from "./ru/loaderPage.json";
+
+import navigationEn from "./en/navigation.json";
+import navigationRu from "./ru/navigation.json";
+
+export const resources = {
+  en: {
+    loaderPage: loaderPageEn,
+    navigation: navigationEn,
+  },
+  ru: {
+    loaderPage: loaderPageRu,
+    navigation: navigationRu,
+  },
+};
