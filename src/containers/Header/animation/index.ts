9c12008546525b97586de3eb29971968871fd7b8
matrizzafox <vas.matrizza@gmail.com>
2023-10-17 14:22:01 +0300

    ➕ADD: Hedaer animation

---
+++ src/containers/Header/animation/index.ts
@@ -0,0 +1,7 @@
+const initialAndExit = { opacity: 0, y: "-100%" };
+
+export const headerAnimation = {
+  initial: initialAndExit,
+  animate: { opacity: 1, y: 0 },
+  exit: initialAndExit,
+};
--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -4,6 +4,8 @@ import WindowActions from "../WindowActions";
 import { useLocation } from "react-router-dom";
 import styles from "./styles/Header.module.scss";
 import HeaderButtons from "@src/components/HeaderButtons";
+import { AnimatePresence, motion } from "framer-motion";
+import { headerAnimation } from "./animation";
 
 const Header = () => {
   const location = useLocation();
@@ -11,9 +13,20 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      {!isLoadingPage && <AppLogo />}
-      {!isLoadingPage && <Navigation />}
-      {!isLoadingPage && <HeaderButtons />}
+      <AnimatePresence>
+        {!isLoadingPage && (
+          <motion.span
+            variants={headerAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            <AppLogo />
+            <Navigation />
+            <HeaderButtons />
+          </motion.span>
+        )}
+      </AnimatePresence>
 
       <WindowActions />
     </header>
