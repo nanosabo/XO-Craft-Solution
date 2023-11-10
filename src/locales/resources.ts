75d869def9e61aaf68b8d3f4ace5310e257ac3e3
sabotage <vas.matrizza@gmail.com>
2023-11-10 15:57:19 +0200

    ➕ADD: Locales for main page

--- src/locales/resources.ts
+++ src/locales/resources.ts
@@ -4,13 +4,18 @@ import loaderPageRu from "./ru/loaderPage.json";
 import navigationEn from "./en/navigation.json";
 import navigationRu from "./ru/navigation.json";
 
+import mainPageEn from "./en/mainPage.json";
+import mainPageRu from "./ru/mainPage.json";
+
 export const resources = {
   en: {
     loaderPage: loaderPageEn,
     navigation: navigationEn,
+    mainPage: mainPageEn,
   },
   ru: {
     loaderPage: loaderPageRu,
     navigation: navigationRu,
+    mainPage: mainPageRu,
   },
 };
