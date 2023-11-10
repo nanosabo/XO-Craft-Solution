fcf3f4fb0ac1a737b403e41a76bad03a995a37a0
sabotage <vas.matrizza@gmail.com>
2023-11-10 15:58:46 +0200

    ➕ADD: Input

---
+++ src/ui/Input/index.tsx
@@ -0,0 +1,19 @@
+import { FC, InputHTMLAttributes, useId } from "react";
+import { SearchIcon } from "../icons";
+import styles from "./styles/Input.module.scss";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+
+const Input: FC<Props> = ({ type, ...props }) => {
+  const id = useId();
+
+  return (
+    <div className={styles.input}>
+      {type === "search" && <SearchIcon />}
+      <input {...props} type={type} id={id} />
+      {type === "checkbox" && <label htmlFor={id}></label>}
+    </div>
+  );
+};
+
+export default Input;
