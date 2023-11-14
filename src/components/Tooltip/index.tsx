1f925dab01461c3eb5305fc9f6e1e193881c00ec
sabotage <vas.matrizza@gmail.com>
2023-11-14 12:47:09 +0200

    ➕ADD: Tooltip component

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import styles from "./styles/Tooltip.module.scss";
+import classNames from "classnames";
+
+type Props = {
+  title: string;
+  subtitle: string;
+  image: string;
+  visible: boolean;
+};
+
+const Tooltip: FC<Props> = ({ title, subtitle, image, visible }) => {
+  return (
+    <div className={classNames(styles.tooltip, { [styles.visible]: visible })}>
+      <p>{title}</p>
+      <span>{subtitle}</span>
+      <img src={image} alt="" draggable="false" />
+    </div>
+  );
+};
+
+export default Tooltip;
